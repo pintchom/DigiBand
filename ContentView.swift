a10ec7c92c10baea366a49a19ed29b0ba8c9647a
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-11-18 13:53:11 -0500

    testing playing multiple sounds at once

--- ContentView.swift
+++ ContentView.swift
@@ -10,19 +10,50 @@ import AVFAudio
 
 struct ContentView: View {
     @StateObject private var bluetoothManager = BluetoothManager()
-    @State private var audioPlayer: AVAudioPlayer!
+    @State private var players = []
+    @State private var audioPlayer1: AVAudioPlayer!
+    @State private var audioPlayer2: AVAudioPlayer!
+    @State private var audioPlayer3: AVAudioPlayer!
+    @State private var audioPlayer4: AVAudioPlayer!
     var body: some View {
         VStack {
             Text(bluetoothManager.isConnected ? "Connected to CPB" : "Searching for CPB...")
                 .padding()
-            
+            HStack {
+                Button {
+                    playSound(soundName: "hat", audioPlayer: 1)
+                } label: {
+                    Text("1")
+                }
+                .buttonStyle(.borderedProminent)
+                Button {
+                    playSound(soundName: "tamborine", audioPlayer: 2)
+                } label: {
+                    Text("2")
+                }
+                .buttonStyle(.borderedProminent)
+                Button {
+                    playSound(soundName: "snare", audioPlayer: 3)
+                } label: {
+                    Text("3")
+                }
+                .buttonStyle(.borderedProminent)
+                Button {
+                    playSound(soundName: "synth", audioPlayer: 4)
+                    playSound(soundName: "snare", audioPlayer: 3)
+                } label: {
+                    Text("4")
+                }
+                .buttonStyle(.borderedProminent)
+                
+            }
             if !bluetoothManager.lastMessage.isEmpty {
                 if bluetoothManager.lastMessage == "A" {
                     Text("Last message: \(bluetoothManager.lastMessage)")
                         .padding()
                         .onAppear {
                             if bluetoothManager.lastMessage == "A" {
-                                playSound(soundName: "alright")
+                                playSound(soundName: "tamborine", audioPlayer: 1)
                             }
                         }
                 } else {
@@ -32,19 +63,29 @@ struct ContentView: View {
             }
         }
     }
-    func playSound(soundName: String) {
+    func playSound(soundName: String, audioPlayer: Int) {
         guard let soundFile = NSDataAsset(name: soundName) else {
             print("Could not read file named: \(soundName)")
             return
         }
         do {
-            audioPlayer = try AVAudioPlayer(data: soundFile.data)
-            audioPlayer.play()
+            if audioPlayer == 1 {
+                audioPlayer1 = try AVAudioPlayer(data: soundFile.data)
+                audioPlayer1.play()
+            } else if audioPlayer == 2 {
+                audioPlayer2 = try AVAudioPlayer(data: soundFile.data)
+                audioPlayer2.play()
+            } else if audioPlayer == 3 {
+                audioPlayer3 = try AVAudioPlayer(data: soundFile.data)
+                audioPlayer3.play()
+            } else if audioPlayer == 4 {
+                audioPlayer4 = try AVAudioPlayer(data: soundFile.data)
+                audioPlayer4.play()
+            }
         } catch {
             print("ERROR: \(error.localizedDescription) creating audio player")
         }
     }
-    
 }
 
 #Preview {
